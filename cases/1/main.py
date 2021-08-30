c31d59835d055bbb7adc2637bd1735b56893e4b0
Simon Ericsson <simsva@protonmail.com>
2021-08-30 23:40:06 +0200

    Cleaned up Case 1

--- cases/1/main.py
+++ cases/1/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import types
 from typing import NamedTuple
 
@@ -13,12 +14,23 @@ class Operation(NamedTuple):
 
 var_prompt = "{var} = "
 
+# List of aliases/bindings
+# Usage and UI are created based on this
 operation_index = {
   "+": 0,
-  "add": 0
+  "add": 0,
+  "-": 1,
+  "sub": 1,
+  "*": 2,
+  "mul": 2,
+  "/": 3,
+  ":": 3,
+  "div": 3,
+  "q": -1,
+  "quit": -1,
 }
 
-# Classes 
+# Operations
 operations = [
   Operation(
     "Addition",
@@ -26,20 +38,53 @@ operations = [
     {'a': float, 'b': float},
     'c',
     lambda x: x['a'] + x['b']
-  )
+  ),
+  Operation(
+    "Subtraction",
+    "a - b = c",
+    {'a': float, 'b': float},
+    'c',
+    lambda x: x['a'] - x['b']
+  ),
+  Operation(
+    "Multiplication",
+    "a * b = c",
+    {'a': float, 'b': float},
+    'c',
+    lambda x: x['a'] * x['b']
+  ),
+  Operation(
+    "Division",
+    "a / b = c",
+    {'a': float, 'b': float},
+    'c',
+    lambda x: x['a'] / x['b']
+  ),
+  # Always last in operation list
+  Operation(
+    "Quit",
+    "This is jank",
+    None,
+    '',
+    None
+  ),
 ]
 
 def generate_textbox(title, body, minlength=0):
   # Get longest line in box or minlength
   length = max(minlength, len(title), max(len(line) for line in body))
+  # Make sure title can be centered
+  if length % 2 != len(title) % 2:
+    length += 1
+  title_pad = ' '*((length-len(title))//2)
 
   formatted_body = '\n'.join([ "| {line: <{length}} |".format(line=line, length=length) for line in body ])
 
   return '''{eq}
-| {title: <{length}} |
+| {title_pad}{title}{title_pad} |
 |{dash}|
 {body}
-{eq}'''.format(eq='='*(length+4), title=title, dash='-'*(length+2), body=formatted_body, length=length)
+{eq}'''.format(eq='='*(length+4), title_pad=title_pad, title=title, dash='-'*(length+2), body=formatted_body, length=length)
 
 def execute_oper(oper):
   print(generate_textbox(
@@ -59,6 +104,7 @@ def execute_oper(oper):
 
   out = oper.func(vars_dict)
   print(var_prompt.format(var=oper.out_var) + str(out))
+  input("Press ENTER to continue...")
 
 def main():
   # Jank way to format aliases to operations
@@ -82,13 +128,16 @@ def main():
     while True:
       oper_index = input(var_prompt.format(var="oper"))
       try:
-        oper = operations[operation_index[oper_index]]
+        oper_id = operation_index[oper_index]
+        oper = operations[oper_id]
         break
       except KeyError:
         print("Invalid operation: '{0}'".format(oper_index))
 
+    # Jank way to exit without extra functionality
+    if oper_id < 0:
+      break
     execute_oper(oper)
-    break
 
 if __name__ == "__main__":
   main()
