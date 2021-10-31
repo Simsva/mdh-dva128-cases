058ae3f8137adb13f7747405d7b0434b7bd95a95
AbbAlgNor <algot.nordstrom@abbindustrigymnasium.se>
2021-10-31 17:53:17 +0100

    2

---
+++ tentamen/1/algot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+
+info = """
+---------------------
+       TODOIFY
+---------------------
+{todos}
+---------------------
+ add | Add todo
+ rm  | Remov todo
+---------------------
+"""
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def main():
+    todos = []
+    while True:
+        cls()
+        print(info.format(todos='\n'.join(["[{}] {}".format(todo[0], todo[1]) for todo in enumerate(todos)])))
+        command = input("action > ").lower()
+        if command == "add": todos.append(input("todo > "))
+        elif command == "rm": 
+            while True:
+                index = input("index > ")
+                try:
+                    del todos[int(index)]
+                    break 
+                except IndexError:
+                    print("Index Does not exist '" + index + "'")
+                except ValueError:
+                    print("Invalid Index '" + index + "'")
+        else: print("invalid command '" + command + "'")
+
+
+if __name__ == "__main__": main()
